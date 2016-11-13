fdf8f646bed95cf810c4eb0d17db218486fd95bc
Eric Ghildyal <ericghildyal@gmail.com>
2016-11-13 03:40:38 -0500

    Some really sweet mongodb code written by AlexKDawson

--- Server/app/routes.js
+++ Server/app/routes.js
@@ -1,12 +1,14 @@
 // app/routes.js
 
 var Spoon = require('./controllers/spoon.js');
+var Send = require('./controllers/send.js');
 
 module.exports = function(app){
 
   app.get('/query', function(req, res){
     Spoon.question(req, function(data){
       // console.log(data);
+      Send.usrdat("Joe Schmoe", 2000, res);
       res.send(data);
     });
   });
--- Server/config/userdb.js
+++ Server/config/userdb.js
@@ -1,5 +1,5 @@
 // config/messagedb.js
 
 module.exports = {
-  url : 'mongodb://admin:admin@jello.modulusmongo.net:27017/ubYq3ipu'
+  url : "mongodb://admin:admin@jello.modulusmongo.net:27017/ubYq3ipu"
 };
