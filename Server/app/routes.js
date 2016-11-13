8a8ffff57daef40ffbf72764c6a115d9030101bd
AlexKDawson <alexdawson@pitt.edu>
2016-11-13 11:51:34 -0500

    FFFFFFFFFFFUUUUUUUUUUUUUUUUUU

--- Server/app/routes.js
+++ Server/app/routes.js
@@ -5,13 +5,14 @@ var Send = require('./controllers/send.js');
 
 module.exports = function(app){
 
-  app.get('/query', function(req, res){
-    Spoon.question(req, function(data){
-      // console.log(data);
-      Send.usrdat("Joe Schmoe", 2000, res);
-      res.send(data);
-    });
-  });
+  // app.get('/query', function(req, res){
+  //   Spoon.question(req, function(data){
+  //     // console.log(data);
+  //     Send.usrdat("Joe Schmoe", 2000, res);
+  //     res.send(data);
+  //   });
+  // });
 
+  app.get(/recipie);
 
 }
--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,7 @@ var bodyParser     = require('body-parser');
 var morgan         = require('morgan');
 var path           = require ('path');
 var mongoose       = require('mongoose');
-var port           = 80;
+var port           = 8080;
 
 //log requests
 // app.use(morgan("combined"));
